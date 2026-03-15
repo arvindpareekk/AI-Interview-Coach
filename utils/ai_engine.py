6ff54e42f4377031bad480f618e8ab76d4b11f75
Arvind Pareek <2022pceadarvind013@poornima.org>
2026-03-15 10:05:38 +0530

    Initial commit - AI Interview Coach

--- utils/ai_engine.py
+++ utils/ai_engine.py
@@ -1,10 +1,23 @@
 from openai import OpenAI
+import os
+from dotenv import load_dotenv
 
-client = OpenAI()
+load_dotenv()
 
-def generate_questions(prompt):
+client = OpenAI(api_key=os.getenv("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"))
+
+def generate_questions(role):
+    
+    prompt = f"""
+    Generate 5 interview questions for the role of {role}.
+    """
+
     response = client.chat.completions.create(
         model="gpt-4o-mini",
-        messages=[{"role": "user", "content": prompt}]
+        messages=[
+            {"role": "system", "content": "You are an expert interviewer."},
+            {"role": "user", "content": prompt}
+        ]
     )
+
     return response.choices[0].message.content
